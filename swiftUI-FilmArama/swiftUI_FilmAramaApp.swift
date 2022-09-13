62e66f117313f71e8252213151c57079e6a2f295
mert <mert.ma346@gmail.com>
2022-09-13 09:36:56 +0300

    Initial Commit

---
+++ swiftUI-FilmArama/swiftUI_FilmAramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_FilmAramaApp.swift
+//  swiftUI-FilmArama
+//
+//  Created by Mert AKBAŞ on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_FilmAramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
